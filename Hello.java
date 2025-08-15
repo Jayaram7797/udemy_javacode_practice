5086c8d23641092489b1625e1f8541acf2b11da0
Jayaram Pemmanaboyidi <jayaram.pemmanaboyidi@gmail.com>
2025-08-16 00:54:45 +0530

    updates

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        int num1 = 3;
+        /* int num1 = 3;
         int num2 = 5;
         int num_sum = num1 + num2;
         System.out.println(num_sum);
@@ -16,14 +16,49 @@ public class Hello {
         boolean b = true;
 
         char c = 'k';
-        char c1 = '8';
+        char c1 = '8'; */
 
-        // write code here
+        /* // write code here
        int age = 25;
        double height = 5.9;
        String name = "Alex";
-       //   System.out.printf("Name: %s, Age: %d, Height: %.1f%n", name, age, height);
-        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height); */
+
+        /* // Literals
+        int num3 = 0b101;
+        int num4 = 0x7E;
+        System.out.println(num3);
+        System.out.println(num4);
+
+        int num5 = 10_00_00_000;
+        double num6 = 12e10;
+        System.out.println(num5);
+        System.out.println(num6);
+
+        char ch = 'a';
+        ch++;
+        System.out.println(ch); */
+
+
+        // Type Conversion and Type Casting
+        // Casting is explicit conversion
+        // Conversuon is automatic conversion
+
+        byte b = 125;
+        int a = 258;
+        byte k = (byte) a; // It does modulus operation
+        System.out.println(k);
+
+        float f = 5.6f;
+        int t = (int) f;
+        System.out.println(t);
+
+        byte p = 10;
+        byte q = 30;
+        int result = p * q; // Type Promotion
+        System.out.println(result);
+
+    
 
     }
 }

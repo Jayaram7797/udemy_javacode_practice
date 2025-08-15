6e7f86e7481262928e1b61525278ae88db1916bd
Jayaram Pemmanaboyidi <jayaram.pemmanaboyidi@gmail.com>
2025-08-16 00:20:17 +0530

    First Commit

---
+++ Hello.java
@@ -0,0 +1,29 @@
+public class Hello {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 5;
+        int num_sum = num1 + num2;
+        System.out.println(num_sum);
+        System.out.println("Hello, World!");
+
+        byte by = 127;
+        short sh = 558;
+        long l = 45678987l; // you must always keep l at the end.
+        System.out.println(by);
+
+        float f = 5.8f; 
+        double d = 5.8;
+        boolean b = true;
+
+        char c = 'k';
+        char c1 = '8';
+
+        // write code here
+       int age = 25;
+       double height = 5.9;
+       String name = "Alex";
+       //   System.out.printf("Name: %s, Age: %d, Height: %.1f%n", name, age, height);
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+
+    }
+}
